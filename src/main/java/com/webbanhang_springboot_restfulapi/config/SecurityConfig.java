c66ab9e9e4c5f6151b3cc0d18432460348991dfd
LeManhHiep <lemanhiep345@gmail.com>
2021-12-09 23:29:10 +0700

    lỗi phần security

---
+++ src/main/java/com/webbanhang_springboot_restfulapi/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.webbanhang_springboot_restfulapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    Ac
+}
